3722f25a66f54b5d679f32b0c618ed6ee51a9329
Yi Wang <yi_wang2@brown.edu>
2019-12-14 00:08:11 +0000

    1

--- get_data.py
+++ get_data.py
@@ -1,30 +1,43 @@
-#!pip3 install beautifulsoup4
-#!pip3 install requests
-#!pip3 install BeautifulSoup4
 from bs4 import BeautifulSoup
 from datetime import datetime
 import requests
-import json
+import sched
+import os.path
+
 url = 'https://www.basketball-reference.com/leagues/NBA_2020_totals.html'
-now = datetime.now()
-save_file = 'data/' + str(now.month) + str(now.day) + '.csv'
 
-html_file = requests.get(url)
-soup = BeautifulSoup(html_file.text)
-table_body = soup.find('tbody')
-rows = table_body.find_all('tr')
-data = []
-feature_names = [row.text.strip() for row in soup.find('thead').find_all("th")]
-data.append(feature_names[1:])
-for row in rows:
-    cols = row.find_all('td')
-    if len(cols):
-        cols = [element.text.strip() for element in cols]
-        data.append([element if element else "NA" for element in cols])
+def _get_file_name(i):
+    now = datetime.now()
+    return 'data/' + str(now.month) + str(now.day) + '_' + str(i) + '.csv'
 
-f = open(save_file,'w',errors = 'ignore')
+def _get_data():
+    html_file = requests.get(url)
+    soup = BeautifulSoup(html_file.text)
+    table_body = soup.find('tbody')
+    rows = table_body.find_all('tr')
+    data = []
+    feature_names = [row.text.strip() for row in soup.find('thead').find_all("th")]
+    data.append(feature_names[1:])
+    for row in rows:
+        cols = row.find_all('td')
+        if len(cols):
+            cols = [element.text.strip() for element in cols]
+            data.append([element if element else "NA" for element in cols])
+    return data
 
-for i in data:
-    f.write(','.join(i)+'\n')
 
-f.close()
+def save_data():
+    name = _get_file_name(1)
+    i = 2
+    while os.path.isfile(name):
+        name = _get_file_name(i)
+        i += 1
+
+    f = open(name, 'w' ,errors = 'ignore')
+    for i in _get_data():
+        f.write(','.join(i)+'\n')
+    f.close()
+    return name
+
+if __name__ == "__main__":
+    save_data()

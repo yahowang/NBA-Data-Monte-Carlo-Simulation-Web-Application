ded4cc2c229d134020b13196366253285401600b
Shiyu Liu <shiyu_liu@brown.edu>
2019-12-15 01:44:55 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -9,60 +9,73 @@ from sklearn.preprocessing import MinMaxScaler
 import numpy as np
 import plotly.graph_objects as go
 from visualization import radar_capability, radar_capability_comparison, \
-    scale_visualization_data, off_def_plot, team_summary
+    off_def_plot, team_summary, scale_player, scale_team
+import pickle
+from data import fantasy_team_stats, Player_ODFAI, results
+from data import C_list, PF_list, PG_list, SF_list, SG_list
+from dash.exceptions import PreventUpdate
 
 
-player_name = ["name1", "name2", "name3", "name4", "name5",
-              "name6", "name7", "name8", "name9", "name10",
-              "name11", "name12", "name13", "name14", "name15"]
-player1 = {"Offensive": 20, "Defensive": 15, "Aggression": 3, "Control":20, "Form Stability": 5}
-player2 = {"Offensive": 16, "Defensive": 30, "Aggression": 4, "Control":28, "Form Stability": 3}
-player3 = {"Offensive": 26, "Defensive": 20, "Aggression": 6, "Control":18, "Form Stability": 2}
-player4 = {"Offensive": 10, "Defensive": 40, "Aggression": 2, "Control":18, "Form Stability": 6}
-player5 = {"Offensive": 14, "Defensive": 30, "Aggression": 3, "Control":13, "Form Stability": 7}
-player6 = {"Offensive": 20, "Defensive": 15, "Aggression": 3, "Control":20, "Form Stability": 5}
-player7 = {"Offensive": 16, "Defensive": 30, "Aggression": 4, "Control":28, "Form Stability": 3}
-player8 = {"Offensive": 26, "Defensive": 20, "Aggression": 6, "Control":18, "Form Stability": 2}
-player9 = {"Offensive": 10, "Defensive": 40, "Aggression": 2, "Control":18, "Form Stability": 6}
-player10 = {"Offensive": 14, "Defensive": 30, "Aggression": 3, "Control":13, "Form Stability": 7}
-player11 = {"Offensive": 20, "Defensive": 15, "Aggression": 3, "Control":20, "Form Stability": 5}
-player12 = {"Offensive": 16, "Defensive": 30, "Aggression": 4, "Control":28, "Form Stability": 3}
-player13 = {"Offensive": 26, "Defensive": 20, "Aggression": 6, "Control":18, "Form Stability": 2}
-player14 = {"Offensive": 10, "Defensive": 40, "Aggression": 2, "Control":18, "Form Stability": 6}
-player15 = {"Offensive": 14, "Defensive": 30, "Aggression": 3, "Control":13, "Form Stability": 7}
+# player_name = ["name1", "name2", "name3", "name4", "name5",
+#               "name6", "name7", "name8", "name9", "name10",
+#               "name11", "name12", "name13", "name14", "name15"]
+# player1 = {"Offensive": 20, "Defensive": 15, "Aggression": 3, "Control":20, "Form Stability": 5}
+# player2 = {"Offensive": 16, "Defensive": 30, "Aggression": 4, "Control":28, "Form Stability": 3}
+# player3 = {"Offensive": 26, "Defensive": 20, "Aggression": 6, "Control":18, "Form Stability": 2}
+# player4 = {"Offensive": 10, "Defensive": 40, "Aggression": 2, "Control":18, "Form Stability": 6}
+# player5 = {"Offensive": 14, "Defensive": 30, "Aggression": 3, "Control":13, "Form Stability": 7}
+# player6 = {"Offensive": 20, "Defensive": 15, "Aggression": 3, "Control":20, "Form Stability": 5}
+# player7 = {"Offensive": 16, "Defensive": 30, "Aggression": 4, "Control":28, "Form Stability": 3}
+# player8 = {"Offensive": 26, "Defensive": 20, "Aggression": 6, "Control":18, "Form Stability": 2}
+# player9 = {"Offensive": 10, "Defensive": 40, "Aggression": 2, "Control":18, "Form Stability": 6}
+# player10 = {"Offensive": 14, "Defensive": 30, "Aggression": 3, "Control":13, "Form Stability": 7}
+# player11 = {"Offensive": 20, "Defensive": 15, "Aggression": 3, "Control":20, "Form Stability": 5}
+# player12 = {"Offensive": 16, "Defensive": 30, "Aggression": 4, "Control":28, "Form Stability": 3}
+# player13 = {"Offensive": 26, "Defensive": 20, "Aggression": 6, "Control":18, "Form Stability": 2}
+# player14 = {"Offensive": 10, "Defensive": 40, "Aggression": 2, "Control":18, "Form Stability": 6}
+# player15 = {"Offensive": 14, "Defensive": 30, "Aggression": 3, "Control":13, "Form Stability": 7}
+#
+# test = pd.DataFrame([player1, player2, player3, player4, player5,
+#                      player6, player7, player8, player9, player10,
+#                      player11, player12, player13, player14, player15], index=player_name)
+#
 
-test = pd.DataFrame([player1, player2, player3, player4, player5,
-                     player6, player7, player8, player9, player10,
-                     player11, player12, player13, player14, player15], index=player_name)
 
-player_df_scaled = scale_visualization_data(test)
+player_cap = pd.read_csv('data/player_cap_df.csv', index_col="Player")
 
+player_name = list(player_cap.index)
 
-player1_od = {"Offensive": 20, "Defensive": 15}
-player2_od = {"Offensive": 16, "Defensive": 30}
-player3_od = {"Offensive": 26, "Defensive": 20}
-player4_od = {"Offensive": 10, "Defensive": 40}
-player5_od = {"Offensive": 14, "Defensive": 30}
-player6_od = {"Offensive": 20, "Defensive": 22}
-player7_od = {"Offensive": 16, "Defensive": 17}
-player8_od = {"Offensive": 26, "Defensive": 23}
-player9_od = {"Offensive": 10, "Defensive": 25}
-player10_od = {"Offensive": 22, "Defensive": 19}
-player11_od = {"Offensive": 17, "Defensive": 18}
-player12_od = {"Offensive": 23, "Defensive": 23}
-player13_od = {"Offensive": 19, "Defensive": 26}
-player14_od = {"Offensive": 11, "Defensive": 22}
-player15_od = {"Offensive": 26, "Defensive": 16}
+# player_df_scaled = scale_visualization_data(player_cap)
 
-test_od = pd.DataFrame([player1_od,player2_od,player3_od,player4_od,player5_od,
-                     player6_od,player7_od,player8_od,player9_od,player10_od,
-                     player11_od,player12_od,player13_od,player14_od,player15_od], index = player_name)
+# player1_od = {"Offensive": 20, "Defensive": 15}
+# player2_od = {"Offensive": 16, "Defensive": 30}
+# player3_od = {"Offensive": 26, "Defensive": 20}
+# player4_od = {"Offensive": 10, "Defensive": 40}
+# player5_od = {"Offensive": 14, "Defensive": 30}
+# player6_od = {"Offensive": 20, "Defensive": 22}
+# player7_od = {"Offensive": 16, "Defensive": 17}
+# player8_od = {"Offensive": 26, "Defensive": 23}
+# player9_od = {"Offensive": 10, "Defensive": 25}
+# player10_od = {"Offensive": 22, "Defensive": 19}
+# player11_od = {"Offensive": 17, "Defensive": 18}
+# player12_od = {"Offensive": 23, "Defensive": 23}
+# player13_od = {"Offensive": 19, "Defensive": 26}
+# player14_od = {"Offensive": 11, "Defensive": 22}
+# player15_od = {"Offensive": 26, "Defensive": 16}
+#
+# test_od = pd.DataFrame([player1_od,player2_od,player3_od,player4_od,player5_od,
+#                      player6_od, player7_od,player8_od,player9_od,player10_od,
+#                      player11_od, player12_od,player13_od,player14_od,player15_od], index = player_name)
+#
+# player_od_df = test_od.copy()
+#
+# team1 = {"Offensive": 20, "Defensive": 15, "Aggression": 3, "Control":20, "Form Stability": 5, "Cooperation": 10}
 
-player_od_df = test_od.copy()
+player_od_df = player_cap[["Offense", "Defense"]]
 
-team1 = {"Offensive": 20, "Defensive": 15, "Aggression": 3, "Control":20, "Form Stability": 5, "Cooperation": 10}
+nclick = 0
 
-################################################3
+################################################
 COLORS = ['rgb(67,67,67)', 'rgb(115,115,115)', 'rgb(49,130,189)', 'rgb(189,189,189)']
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css', '/assets/coff.css']
 
@@ -77,9 +90,9 @@ def page_header():
         html.Div([html.H3('Visualization and Simulation of NBA Player Capability Using 2019-2020 NBA Stats')],
                  className="ten columns"),
         html.A([html.Img(id='logo', src=app.get_asset_url('jack.jpeg'),
-                         style={'height': '35px', 'paddingTop': '7%'}),
+                         style={'height': '35px', 'paddingTop': '7%', 'paddingRight': '300px'}),
                 html.Span('Data1050 Project Git Repo', style={'fontSize': '2rem', 'height': '35px', 'bottom': 0,
-                                                        'paddingLeft': '4px', 'color': '#a3a7b0',
+                                                        'paddingLeft': '1px', 'color': '#a3a7b0',
                                                         'textDecoration': 'none'})],
                className="two columns row",
                href='https://github.com/data1050projectfall2019/data1050project'),
@@ -119,10 +132,6 @@ def description():
         ''', className='eleven columns', style={'paddingLeft': '5%'})], className="row")
 
 
-player_choice = ["name1", "name2", "name3", "name4", "name5",
-              "name6", "name7", "name8", "name9", "name10",
-              "name11", "name12", "name13", "name14", "name15"]
-
 
 def select_one_or_two_player():
     """Select one or two players from the player list"""
@@ -132,7 +141,7 @@ def select_one_or_two_player():
         html.Div(children=[
         dcc.Markdown('''Please select a player to visualize his performance:'''),
         dcc.Dropdown(
-            options=[{"label": name, "value": name} for name in player_choice],
+            options=[{"label": name, "value": name} for name in player_name],
             value=[],
             multi=False,
             id="player1_name",
@@ -142,7 +151,7 @@ def select_one_or_two_player():
         html.Div(children=[
         dcc.Markdown('''Please select a second player to compare their performance:'''),
         dcc.Dropdown(
-            options=[{"label": name, "value": name} for name in player_choice],
+            options=[{"label": name, "value": name} for name in player_name],
             value=[],
             multi=False,
             id="player2_name",
@@ -170,15 +179,22 @@ def enhancement_description():
         ### Fantasy Team
         Finishing viewing the players?  Now select and build your team. You are able to build two teams, and our
         application will be able to visualize the overall team summary using the radar plot. This plot is an 
-        comprehensive estimation on the team you built. 
+        comprehensive estimation on the players you selected. 
         ''', className='eleven columns', style={'paddingLeft': '5%'})], className="row")
 
 
-c = ['name1', 'name2', 'name3']
-pf = ['name4', 'name5', 'name6']
-sf = ['name7', 'name8', 'name9']
-sg = ['name10', 'name11', 'name12']
-pg = ['name13', 'name14', 'name15']
+# c = ['name1', 'name2', 'name3']
+# pf = ['name4', 'name5', 'name6']
+# sf = ['name7', 'name8', 'name9']
+# sg = ['name10', 'name11', 'name12']
+# pg = ['name13', 'name14', 'name15']
+
+
+c = C_list
+pf = PF_list
+sf = SF_list
+sg = SG_list
+pg = PG_list
 
 
 def enhancement_team_what_if():
@@ -292,10 +308,11 @@ def enhancement_team_what_if():
 def start_match():
     return html.Div(children=[
             html.Div(id='above the button', style={'marginTop': '8rem'}),
-            html.Button('Submit', id='button', style={'marginLeft': '450px', 'BackgroundColor': 'white'}),
+            html.Button('Simulate the Match!', id='button', style={'marginLeft': '450px', 'color': 'white', "backgroundColor":'blue'}),
             html.Div(id='output-container-button',
-                     children='Enter a value and press submit')
-            ])
+                     children='After selecting all players, start the match and view the result',
+                     style={'marginLeft': '350px', 'color': 'white', 'fontSize': '2rem'})
+            ], style={'marginLeft': '80px'})
 
 
 def architecture_summary():
@@ -376,50 +393,90 @@ def display_ranking2(player2):
 )
 def what_if_handler(player1, player2):
     """Changes the display graph based on player input"""
-    feature_list = player_df_scaled.columns
     if player1 and player2:
-        cur = radar_capability_comparison(feature_list, player_df_scaled.loc[player1],
-                                          player_df_scaled.loc[player2], [player1, player2])
+        player1_result, feature_list = scale_player(player1, Player_ODFAI(player1))
+        player2_result, feature_list = scale_player(player2, Player_ODFAI(player2))
+        print(player1_result)
+        print(player2_result)
+        cur = radar_capability_comparison(feature_list, player1_result,
+                                          player2_result, [player1, player2])
     elif player1:
-        cur = radar_capability(feature_list,  player_df_scaled.loc[player1], player1)
+        player1_result, feature_list = scale_player(player1, Player_ODFAI(player1))
+        cur = radar_capability(feature_list,  player1_result, player1)
     elif player2:
-        cur = radar_capability(feature_list, player_df_scaled.loc[player2], player2)
+        player2_result, feature_list = scale_player(player2, Player_ODFAI(player2))
+        cur = radar_capability(feature_list, player2_result, player2)
     else:
-        cur = radar_capability(feature_list, [-0.1, -0.1, -0.1, -0.1, -0.1], "No Player Info")
+        cur = radar_capability(['Control', 'Defensive', 'Impact', 'Offense', 'Scoring'], [-0.1, -0.1, -0.1, -0.1, -0.1], "No Player Info")
     return cur
 
 
 @app.callback(
     dash.dependencies.Output('team_summary1', 'figure'),
-    [dash.dependencies.Input("c", 'value'),
-     dash.dependencies.Input("pf", 'value'),
-     dash.dependencies.Input("sf", 'value'),
+    [dash.dependencies.Input("pg", 'value'),
      dash.dependencies.Input("sg", 'value'),
-     dash.dependencies.Input("pg", 'value')]
+     dash.dependencies.Input("sf", 'value'),
+     dash.dependencies.Input("pf", 'value'),
+     dash.dependencies.Input("c", 'value')]
 )
 def display_team1(name1, name2, name3, name4, name5):
+    t0 = {"Offense": -5, "Defense": -5, "Scoring": -5, "Control": -5, "Impact": -5, "Tacit": -5, "Shooting": -5}
     if name1 or name2 or name3 or name4 or name5:
-        cur = team_summary(player1, "TEAM 1")
+        summary0 = fantasy_team_stats(name1, name2, name3, name4, name5)
+        summary1 = scale_team("TEAM 1", summary0)
+        cur = team_summary(summary1, "TEAM 1")
     else:
-        cur = team_summary({"Offensive": 0, "Defensive": 0, "Aggression": 0, "Control": 0,
-                            "Form Stability": 0, "More Feature": 0}, "TEAM 1")
+        cur = team_summary(t0, "TEAM 1")
     return cur
 
 
 @app.callback(
     dash.dependencies.Output('team_summary2', 'figure'),
-    [dash.dependencies.Input("c2", 'value'),
-     dash.dependencies.Input("pf2", 'value'),
-     dash.dependencies.Input("sf2", 'value'),
+    [dash.dependencies.Input("pg2", 'value'),
      dash.dependencies.Input("sg2", 'value'),
-     dash.dependencies.Input("pg2", 'value')]
+     dash.dependencies.Input("sf2", 'value'),
+     dash.dependencies.Input("pf2", 'value'),
+     dash.dependencies.Input("c2", 'value')]
 )
 def display_team2(name1, name2, name3, name4, name5):
+    t0 = {"Offense": -5, "Defense": -5, "Scoring": -5, "Control": -5, "Impact": -5, "Tacit": -5, "Shooting": -5}
     if name1 or name2 or name3 or name4 or name5:
-        cur = team_summary(player2, "TEAM 2")
+        summary0 = fantasy_team_stats(name1, name2, name3, name4, name5)
+        summary2 = scale_team("TEAM 1", summary0)
+        cur = team_summary(summary2, "TEAM 2")
     else:
-        cur = team_summary({"Offensive": 0, "Defensive": 0, "Aggression": 0, "Control": 0, "Form Stability": 0}, "TEAM 2")
+        cur = team_summary(t0, "TEAM 2")
     return cur
+
+
+@app.callback(
+    Output(component_id='output-container-button', component_property='children'),
+    [dash.dependencies.Input(component_id='button', component_property='n_clicks')],
+    [
+     dash.dependencies.State("pg", 'value'),
+     dash.dependencies.State("sg", 'value'),
+     dash.dependencies.State("sf", 'value'),
+     dash.dependencies.State("pf", 'value'),
+     dash.dependencies.State("c", 'value'),
+     dash.dependencies.State("pg2", 'value'),
+     dash.dependencies.State("sg2", 'value'),
+     dash.dependencies.State("sf2", 'value'),
+     dash.dependencies.State("pf2", 'value'),
+     dash.dependencies.State("c2", 'value')
+     ]
+)
+def update_output(n_clicks, player1, player2, player3, player4, player5, player6, player7, player8, player9, player10):
+    if n_clicks is None:
+        raise PreventUpdate
+    else:
+        if player1 and player2 and player3 and player4 and player5 and player6 and player7 and player8 and player9 and player10:
+            ability1, ability2, score1, score2, result = results(player1, player2, player3, player4,
+                                                                 player5, player6, player7, player8, player9, player10)
+            return u'''
+                {}, the score is {} to {}
+            '''.format(result, score1, score2)
+        else:
+            return "You have to select all 10 players"
 
 
 if __name__ == '__main__':
---
+++ data.py
@@ -0,0 +1,272 @@
+import pandas as pd
+import numpy as np
+# from database import *
+
+
+df = pd.read_csv('data/df.csv')
+# df = to_df()
+
+def _cleaning(df):
+    df.loc[df['FGA'] == 0, ['FG%', 'eFG%']] = 0
+    df.loc[df['3PA'] == 0, '3P%'] = 0
+    df.loc[df['2PA'] == 0, '2P%'] = 0
+    df.loc[df['FTA'] == 0, 'FT%'] = 0
+    return df
+
+
+def _Player_ODFI_wopp(Player, Opponent, df):
+    ind = np.where(df['Player'] == Player)[0][0]
+    Team = df['Tm'][ind]
+    Team_df = df[df['Tm'] == Team]
+    Opponent_df = df[df['Tm'] == Opponent].reset_index(drop=True)
+    Team_MP = sum(Team_df['MP'])
+    Team_FGM = sum(Team_df['FG'])
+    Team_FGA = sum(Team_df['FGA'])
+    Team_3PM = sum(Team_df['3P'])
+    Team_FTM = sum(Team_df['FT'])
+    Team_FTA = sum(Team_df['FTA'])
+    Team_ORB = sum(Team_df['ORB'])
+    Team_DRB = sum(Team_df['DRB'])
+    Team_AST = sum(Team_df['AST'])
+    Team_STL = sum(Team_df['STL'])
+    Team_BLK = sum(Team_df['BLK'])
+    Team_TOV = sum(Team_df['TOV'])
+    Team_PF = sum(Team_df['PF'])
+    Team_PTS = sum(Team_df['PTS'])
+
+    Opponent_MP = sum(Opponent_df['MP'])
+    Opponent_FGM = sum(Opponent_df['FG'])
+    Opponent_FGA = sum(Opponent_df['FGA'])
+    Opponent_FTM = sum(Opponent_df['FT'])
+    Opponent_FTA = sum(Opponent_df['FTA'])
+    Opponent_ORB = sum(Opponent_df['ORB'])
+    Opponent_TRB = sum(Opponent_df['TRB'])
+    Opponent_TOV = sum(Opponent_df['TOV'])
+    Opponent_PTS = sum(Opponent_df['PTS'])
+
+    GmPTS = Team_PTS + Opponent_PTS
+    GmFGM = Team_FGM + Opponent_FGM
+    GmFTM = Team_FTM + Opponent_FTM
+    GmFGA = Team_FGA + Opponent_FGA
+    GmFTA = Team_FTA + Opponent_FTA
+    GmDREB = Team_DRB + sum(Opponent_df['DRB'])
+    GmOREB = Team_ORB + Opponent_ORB
+    GmAST = Team_AST + sum(Opponent_df['AST'])
+    GmSTL = Team_STL + sum(Opponent_df['STL'])
+    GmBLK = Team_BLK + sum(Opponent_df['BLK'])
+    GmPF = Team_PF + sum(Opponent_df['PF'])
+    GmTO = Team_TOV + Opponent_TOV
+
+    qAST = ((Team_df.MP / (Team_MP / 5)) * (1.14 * ((Team_AST - Team_df.AST) / Team_FGM))) + ((((
+                                                                                                            Team_AST / Team_MP) * Team_df.MP * 5 - Team_df.AST) / (
+                                                                                                           (
+                                                                                                                       Team_FGM / Team_MP) * Team_df.MP * 5 - Team_df.FG)) * (
+                                                                                                          1 - (
+                                                                                                              Team_df.MP / (
+                                                                                                                  Team_MP / 5))))
+    FG_Part = Team_df.FG * (1 - 0.5 * ((Team_df.PTS - Team_df.FT) / (2 * Team_df.FGA)) * qAST)
+    FG_Part = FG_Part.replace(np.nan, 0)
+    AST_Part = 0.5 * (
+                ((Team_PTS - Team_FTM) - (Team_df.PTS - Team_df.FT)) / (2 * (Team_FGA - Team_df.FGA))) * Team_df.AST
+    FT_Part = (1 - (1 - Team_df['FT%']) ** 2) * 0.4 * Team_df.FTA
+    Team_Scoring_Poss = Team_FGM + (1 - (1 - (Team_FTM / Team_FTA)) ** 2) * Team_FTA * 0.4
+    Team_ORBP = Team_ORB / (Team_ORB + (Opponent_TRB - Opponent_ORB))
+    Team_PlayP = Team_Scoring_Poss / (Team_FGA + Team_FTA * 0.4 + Team_TOV)
+    Team_ORB_Weight = ((1 - Team_ORBP) * Team_PlayP) / ((1 - Team_ORBP) * Team_PlayP + Team_ORBP * (1 - Team_PlayP))
+    ORB_Part = Team_df.ORB * Team_ORB_Weight * Team_PlayP
+    ScPoss = (FG_Part + AST_Part + FT_Part) * (
+                1 - (Team_ORB / Team_Scoring_Poss) * Team_ORB_Weight * Team_PlayP) + ORB_Part
+
+    FGxPoss = (Team_df.FGA - Team_df.FG) * (1 - 1.07 * Team_ORBP)
+    FTxPoss = ((1 - Team_df['FT%']) ** 2) * 0.4 * Team_df.FTA
+
+    TotPoss = ScPoss + FGxPoss + FTxPoss + Team_df.TOV
+
+    PProd_FG_Part = 2 * (Team_df.FG + 0.5 * Team_df['3P']) * (
+                1 - 0.5 * ((Team_df.PTS - Team_df.FT) / (2 * Team_df.FGA)) * qAST)
+    PProd_FG_Part = PProd_FG_Part.replace(np.nan, 0)
+    PProd_AST_Part = 2 * (
+                (Team_FGM - Team_df.FG + 0.5 * (Team_3PM - Team_df['3P'])) / (Team_FGM - Team_df.FG)) * 0.5 * (
+                                 ((Team_PTS - Team_FTM) - (Team_df.PTS - Team_df.FT)) / (
+                                     2 * (Team_FGA - Team_df.FGA))) * Team_df.AST
+    PProd_ORB_Part = Team_df.ORB * Team_ORB_Weight * Team_PlayP * (
+                Team_PTS / (Team_FGM + (1 - (1 - (Team_FTM / Team_FTA)) ** 2) * 0.4 * Team_FTA))
+    PProd = (PProd_FG_Part + PProd_AST_Part + Team_df.FT) * (
+                1 - (Team_ORB / Team_Scoring_Poss) * Team_ORB_Weight * Team_PlayP) + PProd_ORB_Part
+
+    ORtg = 100 * (PProd / TotPoss)
+    ORtg = ORtg.replace(np.nan, 0)
+
+    DORP = Opponent_ORB / (Opponent_ORB + Team_DRB)
+    DFGP = Opponent_FGM / Opponent_FGA
+    FMwt = (DFGP * (1 - DORP)) / (DFGP * (1 - DORP) + (1 - DFGP) * DORP)
+    Stops1 = Team_df.STL + Team_df.BLK * FMwt * (1 - 1.07 * DORP) + Team_df.DRB * (1 - FMwt)
+    Stops2 = (((Opponent_FGA - Opponent_FGM - Team_BLK) / Team_MP) * FMwt * (1 - 1.07 * DORP) + (
+                (Opponent_TOV - Team_STL) / Team_MP)) * Team_df.MP + (Team_df.PF / Team_PF) * 0.4 * Opponent_FTA * (
+                         1 - (Opponent_FTM / Opponent_FTA)) ** 2
+    Stops = Stops1 + Stops2
+    Team_Possessions = sum(TotPoss)
+    StopP = (Stops * Opponent_MP) / (Team_Possessions * Team_df.MP)
+    StopP = StopP.replace(np.nan, 0)
+    Team_Defensive_Rating = 100 * (Opponent_PTS / Team_Possessions)
+    D_Pts_per_ScPoss = Opponent_PTS / (
+                Opponent_FGM + (1 - (1 - (Opponent_FTM / Opponent_FTA)) ** 2) * Opponent_FTA * 0.4)
+    DRtg = Team_Defensive_Rating + 0.2 * (100 * D_Pts_per_ScPoss * (1 - StopP) - Team_Defensive_Rating)
+
+    FloorP = ScPoss / TotPoss
+    FloorP = FloorP.replace(np.nan, 0)
+
+    PIE = (df['PTS'][ind] + df['FG'][ind] + df['FT'][ind] - df['FGA'][ind] - df['FTA'][ind] + df['DRB'][ind] + (
+                .5 * df['ORB'][ind]) + df['AST'][ind] + df['STL'][ind] + (.5 * df['BLK'][ind]) - df['PF'][ind] -
+           df['TOV'][ind]) / (GmPTS + GmFGM + GmFTM - GmFGA - GmFTA + GmDREB + (.5 * GmOREB) + GmAST + GmSTL + (
+                .5 * GmBLK) - GmPF - GmTO)
+
+    return ORtg[ind], DRtg[ind], FloorP[ind], PIE
+
+
+def Player_ODFAI(Player):
+    ind = np.where(df['Player'] == Player)[0][0]
+    Team = df['Tm'][ind]
+    ORtg = 0
+    DRtg = 0
+    FloorP = 0
+    PIE = 0
+    for team in set(df['Tm']):
+        if team != Team:
+            ortg, drtg, floorp, pie = _Player_ODFI_wopp(Player, team, df)
+            ORtg += ortg
+            DRtg += drtg
+            FloorP += floorp
+            PIE += pie
+    num_team = len(set(df['Tm'])) - 1
+    avg_ORtg = ORtg / num_team
+    avg_DRtg = DRtg / num_team
+    avg_FloorP = FloorP / num_team
+    avg_PIE = PIE / num_team
+    if df['TOV'][ind] != 0:
+        AST_TO = df['AST'][ind] / df['TOV'][ind]
+    else:
+        AST_TO = df['AST'][ind] * 2
+
+    ret = {"Offense": avg_ORtg, "Defense": avg_DRtg, "Scoring": avg_FloorP, "Control": AST_TO, "Impact": avg_PIE}
+    return ret
+
+
+def advance_df():
+    # df = _cleaning(to_df())
+    df1 = _cleaning(df)
+    performance = []
+    for player in df1['Player']:
+        performance.append(Player_ODFAI(player, df1))
+
+    advance_df = pd.DataFrame(performance, index = df1['Player'])
+    return advance_df
+
+
+def _Player_PTS_prop(Player, df):
+    ind = np.where(df['Player'] == Player)[0][0]
+    Team = df['Tm'][ind]
+    Team_PTS = sum(df['PTS'][df['Tm'] == Team])
+    PTS_prop = df['PTS'][ind] / Team_PTS
+    return PTS_prop
+
+
+def fantasy_team_stats(PG=None, SG=None, SF=None, PF=None, C=None):
+    # df = to_df()
+    players = []
+    if PG:
+        players.append(PG)
+    if SG:
+        players.append(SG)
+    if SF:
+        players.append(SF)
+    if PF:
+        players.append(PF)
+    if C:
+        players.append(C)
+
+    Tm = []
+    MP = []
+    Offense = []
+    Defense = []
+    Scoring = []
+    Impact = []
+    FGA = 0
+    FTA = 0
+    AST = 0
+    TOV = 0
+    PTS = 0
+    PTS_prop = 0
+    for player in players:
+        ind = np.where(df['Player'] == player)[0][0]
+        Tm.append(df['Tm'][ind])
+        MP.append(df['MP'][ind])
+        ODFAI = Player_ODFAI(player)
+        Offense.append(ODFAI['Offense'])
+        Defense.append(ODFAI['Defense'])
+        Scoring.append(ODFAI['Scoring'])
+        Impact.append(ODFAI['Impact'])
+        FGA += df['FGA'][ind]
+        FTA += df['FTA'][ind]
+        AST += df['AST'][ind]
+        TOV += df['TOV'][ind]
+        PTS += df['PTS'][ind]
+        PTS_prop += _Player_PTS_prop(player, df)
+    MP = np.array(MP)
+    MP_Weight = MP / sum(MP)
+
+    ORtg = sum(MP_Weight * np.array(Offense)) / 2.15 * len(players) / 5
+    DRtg = (sum(MP_Weight * np.array(Defense)) - 90) * 2 * len(players) / 5
+    FloorP = sum(MP_Weight * np.array(Scoring)) * 100 * len(players) / 5
+    PIE = sum(Impact) / 0.0075
+    if PIE < 0:
+        PIE = 0
+    if TOV != 0:
+        AST_TO = AST / TOV * 10
+    else:
+        AST_TO = AST * 2 * 10
+    Tacit = 1.5 - PTS_prop
+    for i in range(len(Tm) - 1):
+        for j in range(i + 1, len(Tm)):
+            if Tm[i] == Tm[j]:
+                Tacit += 0.1
+    Tacit *= 40
+    if FGA == 0 and FTA == 0:
+        Shoot = 0
+    else:
+        Shoot = PTS / (2 * (FGA + 0.44 * FTA)) * 100
+
+    ret = {"Offense": ORtg, "Defense": DRtg, "Scoring": FloorP, "Control": AST_TO, "Impact": PIE, "Tacit": Tacit,
+           "Shooting": Shoot}
+    return ret
+
+
+def results(PG1, SG1, SF1, PF1, C1, PG2, SG2, SF2, PF2, C2):
+    stats1 = fantasy_team_stats(PG1, SG1, SF1, PF1, C1)
+    stats2 = fantasy_team_stats(PG2, SG2, SF2, PF2, C2)
+    ability1 = 0.2 * (stats1['Offense'] + stats1['Defense'] + stats1['Scoring'] + stats1['Control'] + stats1['Tacit'] + stats1['Shooting']) + 0.8 * stats1['Impact']
+    ability2 = 0.2 * (stats2['Offense'] + stats2['Defense'] + stats2['Scoring'] + stats2['Control'] + stats2['Tacit'] + stats2['Shooting']) + 0.8 * stats2['Impact']
+    if ability1 > 100:
+        point1 = 100
+    if ability2 > 100:
+        point2 = 100
+    score1 = int(1.25 * ability1 + np.random.normal(0, 10))
+    score2 = int(1.25 * ability2 + np.random.normal(0, 10))
+    while score1 == score2:
+        score1 += abs(np.random.normal(10, 5))
+        score2 += abs(np.random.normal(10, 5))
+    if score1 > score2:
+        result = "Team 1 wins!"
+    else:
+        result = "Team 2 wins!"
+    return ability1, ability2, score1, score2, result
+
+
+# player_cap = advance_df()
+C_list = list(df['Player'][df['Pos'] == 'C'])
+PF_list = list(df['Player'][df['Pos'] == 'PF'])
+SF_list = list(df['Player'][df['Pos'] == 'SF'])
+SG_list = list(df['Player'][df['Pos'] == 'SG'])
+PG_list = list(df['Player'][df['Pos'] == 'PG'])
+
+
